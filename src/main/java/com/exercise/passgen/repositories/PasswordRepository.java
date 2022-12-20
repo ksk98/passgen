7be7c6644ecaf81427fcc88cb06596d8afca3893
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-12-20 15:48:48 +0100

    Added a finding by hash function to PasswordRepository

--- src/main/java/com/exercise/passgen/repositories/PasswordRepository.java
+++ src/main/java/com/exercise/passgen/repositories/PasswordRepository.java
@@ -1,8 +1,8 @@
 package com.exercise.passgen.repositories;
 
-import com.exercise.passgen.services.PasswordService;
+import com.exercise.passgen.models.entities.PasswordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PasswordRepository extends JpaRepository<Long, PasswordService> {
-
+public interface PasswordRepository extends JpaRepository<PasswordEntity, Long> {
+    boolean existsByPasswordHash(String passwordHash);
 }
