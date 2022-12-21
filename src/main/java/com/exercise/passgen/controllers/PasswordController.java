7da4da3b640dca50c287e77d1ed1d5dd4fd7caff
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-12-21 15:10:59 +0100

    Import cleanup

--- src/main/java/com/exercise/passgen/controllers/PasswordController.java
+++ src/main/java/com/exercise/passgen/controllers/PasswordController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.stream.Collectors;
 
