429482a5eb1c9bcaef8ad46ea194293b1133e3ce
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-12-21 18:56:46 +0100

    PasswordPersistenceTests

--- src/test/java/com/exercise/passgen/Services/PasswordPersistenceTests.java
+++ src/test/java/com/exercise/passgen/Services/PasswordPersistenceTests.java
@@ -6,11 +6,11 @@ import com.exercise.passgen.models.schemas.PasswordDTO;
 import com.exercise.passgen.repositories.PasswordRepository;
 import com.exercise.passgen.services.PasswordService;
 import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
@@ -22,6 +22,7 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class PasswordPersistenceTests {
 
     @Autowired
@@ -29,11 +30,6 @@ public class PasswordPersistenceTests {
 
     @Autowired
     PasswordService passwordService;
-
-    @AfterEach
-    public void cleanupDB() {
-        passwordRepository.deleteAll();
-    }
 
     @Test
     public void SuccessfulPasswordBatchPersistence() throws SearchHashGenerationFailureException {
@@ -118,5 +114,70 @@ public class PasswordPersistenceTests {
         assertEquals(LocalDateTime.of(2020, 4, 15, 5, 5), fromService.getGenerationDateTime());
     }
 
+    @Test
+    public void SuccessfulPasswordPersistenceIgnoreDuplicatesOutsideBatch() throws SearchHashGenerationFailureException {
+        // First batch (unique)
+        List<PasswordDTO> passwords = new ArrayList<>(3);
+        passwords.add(PasswordDTO.builder()
+                .password("example")
+                .complexity(Complexity.LOW)
+                .generationDateTime(LocalDateTime.of(2020, 4, 15, 5, 5))
+                .build());
+        passwords.add(PasswordDTO.builder()
+                .password("example2")
+                .complexity(Complexity.MEDIUM)
+                .generationDateTime(LocalDateTime.of(2021, 4, 15, 5, 5))
+                .build());
+        passwords.add(PasswordDTO.builder()
+                .password("example3")
+                .complexity(Complexity.HIGH)
+                .generationDateTime(LocalDateTime.of(2022, 4, 15, 5, 5))
+                .build());
 
+        List<PasswordDTO> duplicates = passwordService.persistUniquePasswords(passwords);
+        assertEquals(0, duplicates.size());
+
+        // Second batch (with duplicates)
+        passwords.clear();
+        passwords.add(PasswordDTO.builder()
+                .password("example")
+                .complexity(Complexity.MEDIUM)
+                .generationDateTime(LocalDateTime.of(2025, 4, 15, 5, 5))
+                .build());
+        passwords.add(PasswordDTO.builder()
+                .password("example4")
+                .complexity(Complexity.ULTRA)
+                .generationDateTime(LocalDateTime.of(2027, 4, 15, 5, 5))
+                .build());
+        passwords.add(PasswordDTO.builder()
+                .password("example3")
+                .complexity(Complexity.LOW)
+                .generationDateTime(LocalDateTime.of(2026, 4, 15, 5, 5))
+                .build());
+
+        duplicates = passwordService.persistUniquePasswords(passwords);
+        assertEquals(2, duplicates.size());
+        assertEquals("example", duplicates.get(0).getPassword());
+        assertEquals("example3", duplicates.get(1).getPassword());
+
+        PasswordDTO fromService = passwordService.getPasswordDTO("example");
+        assertNotNull(fromService);
+        assertEquals(Complexity.LOW, fromService.getComplexity());
+        assertEquals(LocalDateTime.of(2020, 4, 15, 5, 5), fromService.getGenerationDateTime());
+
+        fromService = passwordService.getPasswordDTO("example2");
+        assertNotNull(fromService);
+        assertEquals(Complexity.MEDIUM, fromService.getComplexity());
+        assertEquals(LocalDateTime.of(2021, 4, 15, 5, 5), fromService.getGenerationDateTime());
+
+        fromService = passwordService.getPasswordDTO("example3");
+        assertNotNull(fromService);
+        assertEquals(Complexity.HIGH, fromService.getComplexity());
+        assertEquals(LocalDateTime.of(2022, 4, 15, 5, 5), fromService.getGenerationDateTime());
+
+        fromService = passwordService.getPasswordDTO("example4");
+        assertNotNull(fromService);
+        assertEquals(Complexity.ULTRA, fromService.getComplexity());
+        assertEquals(LocalDateTime.of(2027, 4, 15, 5, 5), fromService.getGenerationDateTime());
+    }
 }
