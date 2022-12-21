286cc3aabac6d00a8202f0c4d3bb5c0f6473ac4f
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-12-21 15:06:11 +0100

    Moved Complexity rules to the enum itself, moved general password rules to PasswordRules

--- src/main/java/com/exercise/passgen/enums/Complexity.java
+++ src/main/java/com/exercise/passgen/enums/Complexity.java
@@ -1,7 +1,11 @@
 package com.exercise.passgen.enums;
 
 import com.exercise.passgen.PasswordRules;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public enum Complexity {
     ULTRA(17, true, true),
     HIGH(9, true, true),
@@ -11,12 +15,6 @@ public enum Complexity {
 
     public final int MINIMUM_CHARACTERS;
     public final boolean REQUIRES_LOWER_AND_UPPER, REQUIRES_SPECIAL;
-
-    Complexity(int MINIMUM_CHARACTERS, boolean REQUIRES_LOWER_AND_UPPER, boolean REQUIRES_SPECIAL) {
-        this.MINIMUM_CHARACTERS = MINIMUM_CHARACTERS;
-        this.REQUIRES_LOWER_AND_UPPER = REQUIRES_LOWER_AND_UPPER;
-        this.REQUIRES_SPECIAL = REQUIRES_SPECIAL;
-    }
 
     public boolean matchesCriteria(int characters, boolean lowerCase, boolean upperCase, boolean specialCase) {
         return characters >= MINIMUM_CHARACTERS &&
