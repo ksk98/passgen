818e08cd942cf4cbe384431a7bd33703bd8aebd1
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-12-21 13:20:06 +0100

    Fixed jackson deserialization problem

--- src/main/java/com/exercise/passgen/models/schemas/PasswordGenerationResponseDTO.java
+++ src/main/java/com/exercise/passgen/models/schemas/PasswordGenerationResponseDTO.java
@@ -1,13 +1,17 @@
 package com.exercise.passgen.models.schemas;
 
 import com.exercise.passgen.enums.Complexity;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PasswordGenerationResponseDTO {
     private List<String> passwords, duplicates;
     private Complexity complexity;
