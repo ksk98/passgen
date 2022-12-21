ef0a39bb24c7abade830fd82bfb9196ec8a39477
Krzysztof Krawczyk <krzysztofskrawczyk@gmail.com>
2022-12-21 16:09:24 +0100

    Fixed no support for LocalDateTime in jackson

--- src/main/java/com/exercise/passgen/models/schemas/PasswordDTO.java
+++ src/main/java/com/exercise/passgen/models/schemas/PasswordDTO.java
@@ -1,6 +1,10 @@
 package com.exercise.passgen.models.schemas;
 
 import com.exercise.passgen.enums.Complexity;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,5 +19,7 @@ import java.time.LocalDateTime;
 public class PasswordDTO {
     private String password;
     private Complexity complexity;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime generationDateTime;
 }
